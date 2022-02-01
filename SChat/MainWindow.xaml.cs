3f8852a302280581743c38451195dc9c08b53e03
Артем Гавриленко <milick33@gmail.com>
2022-02-01 14:27:17 +0700

    Добавьте файлы проекта.

---
+++ SChat/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SChat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Grid adf = new Grid();
+            adf.Margin = new Thickness(10);
+            adf.Width = 130;
+            adf.Height = 40;
+            Image afsd = new Image();
+            afsd.Margin = image.Margin;
+            afsd.HorizontalAlignment = image.HorizontalAlignment;
+            afsd.Width = image.Width;
+            afsd.Height = image.Height;
+            afsd.Source = image.Source;
+            adf.Children.Add(afsd);
+
+            dsg.Children.Add(adf);
+        }
+    }
+}
